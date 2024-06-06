b2aff06c24899648aee6be75402653421f51c521
Vishnu Prasad Ayyalasomayajula <vishnua@gmail.com>
2024-06-06 15:47:16 -0400

    Updated

--- crd-pmd-camel-processor/src/main/java/com/frk/crd/camel/CamelRoute.java
+++ crd-pmd-camel-processor/src/main/java/com/frk/crd/camel/CamelRoute.java
@@ -1,12 +1,8 @@
 package com.frk.crd.camel;
 
-import com.frk.crd.model.WFEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jms.JmsComponent;
-import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -32,29 +28,10 @@ public class CamelRoute extends RouteBuilder {
   @Override
   public void configure() {
     getContext().setTypeConverterStatisticsEnabled(true);
-    getContext().addComponent(ACTIVEMQ_COMPONENT, JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
-    from(ACTIVEMQ_COMPONENT + COMPONENT_SEPARATOR + inQueue)
-      .unmarshal().json(JsonLibrary.Jackson, WFEvent.class)
-      .choice()
-      .when().simple("${body.inputEvent} == 'PDP-ORDER-STATUS'").setHeader("CRDEvent", () -> "PDP-ORDER-STATUS")
-      .otherwise().setHeader("CRDEvent", () -> "Not-Available")
-      .to(ACTIVEMQ_COMPONENT + COMPONENT_SEPARATOR + outQueue)
-      .to("log:com.frk.crd?level=INFO&groupSize=10");
-
-    from(ACTIVEMQ_COMPONENT + COMPONENT_SEPARATOR + outQueue).to("log:com.frk.crd?level=INFO&groupSize=10")
-      .onException(Throwable.class).process(new Processor() {
-        @Override
-        public void process(Exchange exchange) throws Exception {
-          log.error("Unable to process message {}", exchange.getIn(), exchange.getException());
-
-          // send to exception queue
-          // update message in DB as failed status
-        }
-      });
-
-//        onException()
-//        from("activemq:" + inQueue)
-//                .unmarshal().jacksonxml()
-//                .to("log:com.frk.crd?level=INFO&groupSize=10");
+    getContext().addComponent("activemq", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+    from("activemq:" + inQueue)
+      .to("activemq:" + outQueue)
+      .to("log:com.frk.crd?level=INFO&groupSize=10")
+      .log("Found message in queue " + inQueue);
   }
 }
--- crd-pmd-camel-processor/src/main/java/com/frk/crd/model/EventWrapper.java
+++
@@ -1,8 +0,0 @@
-package com.frk.crd.model;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-
-public class EventWrapper implements XMLParsingEligible {
-  @JsonAlias("event")
-  private Event event;
-}
